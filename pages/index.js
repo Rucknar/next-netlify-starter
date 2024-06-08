7d0d5cc386f3ab0ae8e998e3ea437a92d9791481
Ed Marshall <ruckafella@gmail.com>
2024-06-08 12:42:09 +0100

    Holding page

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,38 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Vintaga Ltd.</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <h1 className="title">Welcome to Vintaga Ltd.</h1>
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          We're excited to be launching soon, watch this space.
         </p>
       </main>
 
-      <Footer />
+      <style jsx>{`
+        .container {
+          min-height: 100vh;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+          text-align: center;
+        }
+        .title {
+          font-size: 4rem;
+          margin: 0;
+        }
+        .description {
+          font-size: 1.5rem;
+          margin: 0.5rem 0;
+        }
+      `}</style>
     </div>
-  )
+  );
 }
